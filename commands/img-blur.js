5efaac1e8901dc8b1f6492bcacfa90da817b93ea
LordGaaju001 <chrisgaajukpeme2027@gmail.com>
2026-02-12 15:30:04 +0100

    Update img-blur.js

--- commands/img-blur.js
+++ commands/img-blur.js
@@ -65,7 +65,7 @@ async function blurCommand(sock, chatId, message, quotedMessage) {
                 forwardingScore: 1,
                 isForwarded: true,
                 forwardedNewsletterMessageInfo: {
-                    newsletterJid: '120363406735242612@newsletter',
+                    newsletterJid: '120363406588763460@newsletter',
                     newsletterName: 'Zenitsu Bot',
                     serverMessageId: -1
                 }
